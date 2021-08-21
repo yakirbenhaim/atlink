6fe6974ef17e2868c89cb0e3393842a308e9fd67
yakirbenhaim <73297504+yakirbenhaim@users.noreply.github.com>
2021-08-21 17:33:47 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,39 +1,38 @@
-import time
+import os
+
+from flask import Flask, request
+
 import telebot
 
-TOKEN = "your bot token here"
-bot = telebot.TeleBot(token=TOKEN)
+TOKEN = '1907710999:AAGYyoRj7dQiaBKWzI6U0l-vADpZJygFA54'
+bot = telebot.TeleBot(TOKEN)
+server = Flask(__name__)
 
-def findat(msg):
-    # from a list of texts, it finds the one with the '@' sign
-    for i in msg:
-        if '@' in i:
-            return i
 
-@bot.message_handler(commands=['start']) # welcome message handler
-def send_welcome(message):
-    bot.reply_to(message, '(placeholder text)')
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
-@bot.message_handler(commands=['help']) # help message handler
-def send_welcome(message):
-    bot.reply_to(message, 'ALPHA = FEATURES MAY NOT WORK')
 
-@bot.message_handler(func=lambda msg: msg.text is not None and '@' in msg.text)
-# lambda function finds messages with the '@' sign in them
-# in case msg.text doesn't exist, the handler doesn't process it
-def at_converter(message):
-    texts = message.text.split()
-    at_text = findat(texts)
-    if at_text == '@': # in case it's just the '@', skip
-        pass
-    else:
-        insta_link = "https://instagram.com/{}".format(at_text[1:])
-        bot.reply_to(message, insta_link)
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
 
-while True:
-    try:
-        bot.polling(none_stop=True)
-        # ConnectionError and ReadTimeout because of possible timout of the requests library
-        # maybe there are others, therefore Exception
-    except Exception:
-        time.sleep(15)
+
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://pythonggdfgdf.herokuapp.com/' + TOKEN)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
